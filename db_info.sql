a036ea6144606f972515043bf4911ba8b1b165d8
navyad <navyad.pro@gmail.com>
2020-11-23 22:08:21 +0530

    db size query

--- db_info.sql
+++ db_info.sql
@@ -1,2 +1,6 @@
 -- https://www.postgresql.org/docs/current/catalog-pg-database.html
 select *  FROM pg_database;
+
+
+-- size of all dbs
+SELECT pg_database.datname, pg_size_pretty(pg_database_size(pg_database.datname)) AS size FROM pg_database;
